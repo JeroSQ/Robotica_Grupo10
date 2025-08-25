80c511c38a21e2093d1b71a6cd8499d644df719d
Jerónimo Squartini <jerosquartini123@gmail.com>
2025-08-25 18:24:33 -0300

    creé la matriz con los parámetros de denavit hartenberg

---
+++ Trabajo_Final/plotter.m
@@ -0,0 +1,4 @@
+robot
+
+R.plot(q,'scale',0.8,'trail',{'r', 'LineWidth', 2})
+R.teach()
--- Trabajo_Final/robot.m
+++ Trabajo_Final/robot.m
@@ -1,3 +1,17 @@
 % robot.m para el trabajo final
 % ROBOT KUKA KR 30 R2100
 clc, clear, close all
+
+dh = [
+    0.000  0.575  0.175  -pi/2 0;
+    0.000  0.000  0.890  0.000 0;
+    0.000  0.000  0.000  -pi/2 0;
+    0.000  1.035  0.050  pi/2 0;
+    0.000  0.000  0.000  -pi/2 0;
+    0.000  0.185  0.000  0.000 0];
+
+% setear los parámetros que faltan
+R = SerialLink(dh,'name','KUKA KR 30 2100');
+q = [0,0,0,0,0,0];
+
+% definir la variable workspace
