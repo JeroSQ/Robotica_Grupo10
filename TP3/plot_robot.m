3c222606785b00dfc41cb409110ea1b030479b3b
Jerónimo Squartini <jerosquartini123@gmail.com>
2025-08-29 22:01:17 -0300

    arregle plot

--- TP3/plot_robot.m
+++ TP3/plot_robot.m
@@ -1,32 +1,27 @@
 clc, clear, close all;
 robot;
 
-% b. Definición de un vector de posiciones articulares a analizar
-q = [0, 0, 0, 0, 0, 0];  % Configuración home (todas las articulaciones en cero)
+% b. Definición de un vector de posiciones articulares
+q = [0, -pi/4, 0, pi/4, pi/3, 0]; 
 
 % c. Definición de un vector de booleanos para sistemas de referencia
 % [sistema{0}, sistema{1}, sistema{2}, sistema{3}, sistema{4}, sistema{5}, sistema{6}]
 sistemas = [1, 1, 1, 1, 1, 1, 1];  % 1 = visualizar, 0 = ocultar
 
-% d. Ploteo del robot con las propiedades recomendadas
+% d. Ploteo del robot
 figure('Name', 'Sistemas de Referencia DH', 'NumberTitle', 'off');
 R.plot(q, 'scale', 0.5, 'jointdiam', 0.8, 'notiles', 'workspace', workspace);
-%jointdiam', 0.8,  Reduce el diámetro de las articulaciones
-%'notiles',  Elimina el suelo de la visualización
 hold on;
 
-% e. Bucle para graficar sistemas de referencia según el vector de booleanos
-for i = 0:R.n
+% e. Bucle para graficar sistemas de referencia
+[T,all]=R.fkine(q);
+for i = 1:R.n
     if sistemas(i+1) == 1
-        if i == 0
-            T = eye(4);   % Sistema base {0}
+        if i==1
+            trplot(R.base,'length',0.5,'frame',num2str(i-1),'color','k');
         else
-            T = R.A(1, q);   % inicializamos con el primero
-            for j = 2:i
-                T = T * R.A(j, q);
-            end
+            trplot(all(i-1),'length',0.5,'frame',num2str(i-1),'color','k');
         end
-        trplot(T, 'frame', num2str(i), 'color', 'r', 'length', 0.2, 'thick', 2);
     end
 end
 
