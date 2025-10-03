f6e76a077d88ecb36e2593ec20325a7584e874a1
mikabergamin-alt <mikabergamin@gmail.com>
2025-10-03 20:13:47 -0300

    anashe

--- TP5_B/Ejercicio1_5.m
+++ TP5_B/Ejercicio1_5.m
@@ -1,21 +1,21 @@
 function [Q123, T03_list] = Ejercicio1_5(R, pc)
-% Calcula las 4 posibles soluciones (q1, q2, q3) para ubicar el centro
-% de la muñeca en pc = [xc; yc; zc].
+% Calcula las 4 posibles combinaciones (q1, q2, q3) que ubican
+% el centro de la muñeca en el punto pc = [xc; yc; zc].
 
-xc = pc(1); 
-yc = pc(2); 
-zc = pc(3); % zc no afecta a q1-q2-q3 en este robot
+xc = pc(1);
+yc = pc(2);
+zc = pc(3); % zc no influye en q1, q2 y q3 para este robot
 
-% --- Posibles valores de q1 (frente y opuesto)
+% --- Dos posibles valores para q1 (frente y opuesto)
 q1A = atan2(yc, xc);
 if q1A > 0
     q1B = q1A - pi;
 else
     q1B = q1A + pi;
 end
-q1_all = [q1A, q1B];
+q1_todos = [q1A, q1B];
 
-% --- Longitudes de los eslabones 2 y 3
+% --- Longitudes de los eslabones
 L2 = R.links(2).a;
 L3 = R.links(3).a;
 if abs(L3) < eps
@@ -29,26 +29,26 @@ end
 
 c = 1;
 for k = 1:2
-    q1 = q1_all(k);
+    q1 = q1_todos(k);
     T1 = R.links(1).A(q1).double;
-    p1 = invHomog(T1) * [pc;1]; 
+    p1 = invHomog(T1) * [pc;1];
     p1 = p1(1:3);
-    x1 = p1(1);  
+    x1 = p1(1);
     y1 = p1(2);
 
-    % Distancia al segundo eslabón
+    % Distancia desde el eje del segundo eslabón
     r = hypot(x1, y1);
     B = atan2(y1, x1);
 
-    % --- Ley de cosenos para q2
+    % --- Cálculo de q2 con ley de cosenos
     arg = (L2^2 + r^2 - L3^2) / (2*r*L2);
-    arg = max(-1, min(1, arg)); % limitar por errores numéricos
+    arg = max(-1, min(1, arg)); % evita errores numéricos
     G = acos(arg);
-    q2_candidates = [B - G, B + G]; % codo arriba y codo abajo
+    q2_posibles = [B - G, B + G]; % codo arriba y codo abajo
 
-    % --- Para cada q2 calculo q3
+    % --- Para cada q2 se calcula q3
     for i = 1:2
-        q2 = q2_candidates(i);
+        q2 = q2_posibles(i);
         T2 = T1 * R.links(2).A(q2).double;
         p2 = invHomog(T2) * [pc;1];
         p2 = p2(1:3);
