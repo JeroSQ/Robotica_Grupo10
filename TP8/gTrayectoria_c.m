dd7ddcb39f6fe940919d868c0ae36c724c8bcfd1
gsg1234 <gonzalogantus@hotmail.com>
2025-10-24 22:34:03 -0300

    TP8EjFinal

---
+++ TP8/gTrayectoria_c.m
@@ -0,0 +1,14 @@
+function [q, qd, qdd] = gTrayectoria_c(listaTs, R,qq)
+    q=[];
+    qd=[];
+    qdd=[];
+    qant=R.ikine(listaTs{1}, qq);
+    for i = 2:numel(listaTs)
+        Tinters=ctraj(listaTs{i-1}, listaTs{i}, 100);
+        qaux=R.ikine(Tinters, qq);
+        q=[q;qaux];
+    end
+   qd  = gradient(q);
+   qdd = gradient(qd);
+   qq=qaux;
+end
