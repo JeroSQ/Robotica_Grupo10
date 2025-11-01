44efd5ed16fdb96b1bbc0740542522a2db2a47af
gsg1234 <gonzalogantus@hotmail.com>
2025-11-01 16:02:53 -0300

    Trayectorias ordenadas

--- TFinal/trayectorias.m
+++ TFinal/trayectorias.m
@@ -2,50 +2,40 @@ clear; clc;
 robotA;
 robotB;
 
-%robotAenEspera
+%% Trayectorias calculadas %%
+% 1: RA Llega a T5
+% 2: RB se acomoda con la rueda a la derecha
+% 3: RA saca la rueda
+% 4: RA sale hacia la izquierda y RB entra para hacer el cambio
+% 5: RB coloca la nueva rueda
+% 6: RA deja la rueda vieja a un lado y hace homming
+% 7: RB hace homming
+
+%Los pasos 1 y 2 y, 6 y 7 se pueden realizar de manera asincrona o sincrona
+%El paso 4 es el unico que verdaderamente necesita sincronismo
+
+%% Definicion de los T antes de los pasos %%
 qqA=[pi/2,0,0,0,0,0];
-T0A=RA.fkine(qqA);
+qqB=[-pi/2,0,0,0,0,0];
+
 P5 = [1.5 0 0.46];
+
+P1A =[0 2 1.515];
+P2A = [0 2  0.3525];
+P3A = [0 2  0.1525];
+P4A = [1.5 0.8 0.46];
+
+P1B =[0 -2 1.515];
+P2B = [0 -2  0.3525];
+P3B = [0 -2  0.1525];
+P4B = [1.-5 0.8 0.46];
+
 T5 = eye(4);
 T5(1:3,4) = P5;
 T5(1:3, 1:3) = [0 0 1; 0 -1 0; 1 0 0];
 
-Ts1A={T0A,T5};
-
-[q1A,qd1A,qdd1A,qqA]=gTrayectoria_a(Ts1A,RA,qqA);
-
-%robotBenEspera
-
-qqB=[-pi/2,0,0,0,0,0];
-T0B=RB.fkine(qqB);    
-P1B = [0 -2 1.515];
-P2B = [0 -2  0.3525];
-P3B= [0 -2  0.1525];
-T1B=trotx(pi);
-T1B(1:3,4)=P1B;
-
-T2B = T1B;
-T2B(1:3,4) = P2B;
-
-T3B=T2B;
-T3B(1:3,4)=P3B;
-
-Ts1B={T0B,T1B,T2B};
-Tsc1B={T2B,T3B};
-
-[q1aB,qd1aB,qdd1aB,qqB]=gTrayectoria_a(Ts1B,RB,qqB);
-[q1cB,qd1cB,qdd1cB,qqB]=gTrayectoria_c(Tsc1B,RB,qqB);
-
-q1B=[q1aB;q1cB];
-
-%Llega el auto y comienza el cambio
-
-P1A =[0 2 1.515];
-P2A = [0 2  0.3525];
-P3A= [0 2  0.1525];
-
-%PUNTO AUXILIAR HASTA TENER EL REAL
-Paux=[2 0 0.46];
+T0A=RA.fkine(qqA);
+T0B=RB.fkine(qqB);
 
 T1A=trotx(pi);
 T1A(1:3,4)=P1A;
@@ -53,42 +43,75 @@ T1A(1:3,4)=P1A;
 T2A = T1A;
 T2A(1:3,4) = P2A;
 
-T3A=T2A;
-T3A(1:3,4)=P3A;
+T3A = T2A;
+T3A(1:3,4) = P3A;
 
-%T_rueda=graficar
+T4A=T5;
+T4A(2,4) = 0.8;
+
+T1B=trotx(pi);
+T1B(1:3,4) = P1B;
+
+T2B = T1B;
+T2B(1:3,4) = P2B;
+
+T3B = T2B;
+T3B(1:3,4) = P3B;
+
+T4B=T5;
+T4B(2,4) = - 0.8;
+
+%PUNTO AUXILIAR HASTA TENER EL REAL
+Paux=[2 0 0.46];
+
 T_rueda=T5;
 T_rueda(1:3,4)=Paux;
 T_FL_40cm = T_rueda;
 T_FL_40cm(1:3,4) = T_rueda(1:3,4) - T_rueda(1:3,3) * 0.4;
 
-T_BEspera_40cm=T_FL_40cm;
-T_BEspera_40cm(2,4) = T_FL_40cm(2,4) - 0.8;
 
-T_AEspera_40cm=T_FL_40cm;
-T_AEspera_40cm(2,4) = T_FL_40cm(2,4) + 0.8;
+%% PASO 1 %%
+Ts1A={T0A,T5};
+[q1A,qd1A,qdd1A,qqA]=gTrayectoria_a(Ts1A,RA,qqA);
 
-Ts2A={T5,T_FL_40cm,T_rueda,T_FL_40cm};
-Ts2B={T3B,T2B,T_BEspera_40cm,T_BEspera_40cm};
+%% PASO 2 %%
+Ts2Ba1={T0B,T1B,T2B};
+Ts2Bc={T2B,T3B};
+Ts2Ba2={T3B,T2B,T4B};
 
-Ts3A={T_FL_40cm,T_AEspera_40cm,T3A,T2A};
-Ts3B={T_BEspera_40cm,T_FL_40cm,T_rueda,T_FL_40cm};
+[q2aB,qd2aB,qdd2aB,qqB]=gTrayectoria_a(Ts2Ba1,RB,qqB);
+[q2cB,qd2cB,qdd2cB,qqB]=gTrayectoria_c(Ts2Bc,RB,qqB);
+[q2dB,qd2dB,qdd2dB,qqB]=gTrayectoria_a(Ts2Ba2,RB,qqB);
 
-[qR1A,qdR1A,qddR1A,qqA]=gTrayectoria_c(Ts2A,RA,qqA);
-[qR1B,qdR1B,qddR1B,qqB]=gTrayectoria_a(Ts2B,RB,qqB);
+q2B=[q2aB;q2cB;q2dB];
+qd2B=[qd2aB;qd2cB;qd2dB];
+qdd2B=[qdd2aB;qdd2cB;qdd2dB];
 
-[qR2A,qdR2A,qddR2A,qqA]=gTrayectoria_a(Ts3A,RA,qqA);
-[qR2B,qdR2B,qddR2B,qqB]=gTrayectoria_c(Ts3B,RB,qqB);
+%% PASO 3 %%
+Ts3A={T5,T_FL_40cm,T_rueda,T_FL_40cm};
+[q3A,qd3A,qdd3A,qqA]=gTrayectoria_c(Ts3A,RA,qqA);
+%% PASO 4 %%
+Ts4A={T_FL_40cm,T4A};
+Ts4B={T4B,T_FL_40cm};
+[q4A,qd4A,qdd4A,qqA]=gTrayectoria_a(Ts4A,RA,qqA);
+[q4B,qd4B,qdd4B,qqA]=gTrayectoria_a(Ts4B,RB,qqA);
+%% PASO 5 %%
+Ts5B={T_FL_40cm,T_rueda,T_FL_40cm};
+[q5B,qd5B,qdd5B,qqA]=gTrayectoria_a(Ts5B,RB,qqA);
+%% PASO 6 %%
+Ts6Aa2={T2A,T1A,T0A};
+Ts6Ac={T3A,T2A};
+Ts6Aa1={T4A,T2A,T3A};
 
-q2A=[qR1A;qR2A];
-q2B=[qR1B;qR2B];
+[q6aA,qd6aA,qdd6aA,qqA]=gTrayectoria_a(Ts6Aa1,RA,qqA);
+[q6cA,qd6cA,qdd6cA,qqA]=gTrayectoria_c(Ts6Ac,RA,qqA);
+[q6dA,qd6dA,qdd6dA,qqA]=gTrayectoria_a(Ts6Aa2,RA,qqA);
 
-%Homming Final
+q6A=[q6aA;q6cA;q6dA];
+qd6A=[qd6aA;qd6cA;qd6dA];
+qdd6A=[qdd6aA;qdd6cA;qdd6dA];
+%% PASO 7 %%
+Ts7B={T_FL_40cm,T0B};
+[q7B,qd7B,qdd7B,qqB]=gTrayectoria_a(Ts7B,RB,qqB);
 
-Ts3A={T2A,T0A};
-Ts3B={T_FL_40cm,T0B};
-
-[q3A,qd3A,qdd3A,qqA]=gTrayectoria_a(Ts3A,RA,qqA);
-[q3B,qd3B,qdd3B,qqB]=gTrayectoria_a(Ts3B,RB,qqB);
-
-save('trayectorias_robots.mat','q1A','q1B','q2A','q2B','q3A','q3B');
+save('trayectorias_robots.mat','q1A','q2B','q3A','q4A','q4B','q5B','q6A','q7B');
