23f8409e677c29b0cd9f9aa20986714d03f3650f
tomasagustingarciasad-cmyk <tomasagustingarciasad@yahoo.com.ar>
2025-11-05 12:18:33 -0300

    Se corrigio uno de los metodos de calculo de trayectoria en donde haciamos uso de jtraj y tenia que ser ctraj

--- TFinal/trayectorias.m
+++ TFinal/trayectorias.m
@@ -119,8 +119,8 @@ Ts4B={T4B,T_FL_40cm};
 %% PASO 5 %%
 Ts5B0={T_FL_40cm,T_rueda};
 Ts5B1={T_rueda,T_FL_40cm};
-[q5B0,qd5B0,qdd5B0,qqA]=gTrayectoria_a(Ts5B0,RB,qqA);
-[q5B1,qd5B1,qdd5B1,qqA]=gTrayectoria_a(Ts5B1,RB,qqA);
+[q5B0,qd5B0,qdd5B0,qqA]=gTrayectoria_c(Ts5B0,RB,qqA);
+[q5B1,qd5B1,qdd5B1,qqA]=gTrayectoria_c(Ts5B1,RB,qqA);
 %% PASO 6 %%
 Ts6Aa2={T2A,T1A,T0A};
 Ts6Ac={T3A,T2A};
